633f6acb6af7a0bad10818137591e968529769f0
Askarbekov Aktan <akn392204@gmail.com>
2024-03-21 18:41:26 +0600

    Added methods and Main in first task where we work with Thread library

---
+++ Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿int totalTime = 0; 
+
+Thread vasyaThread = new Thread(VasyasMorningRoutine);
+vasyaThread.Start();
+vasyaThread.Join(); 
+Console.WriteLine($"Общее затраченное время: {totalTime} минут");
+
+void VasyasMorningRoutine()
+{
+    totalTime += WaitAndReturnTime(15, "Просыпается и лежит в кровати");
+    totalTime += WaitAndReturnTime(5, "Ставит чайник на плиту");
+    totalTime += WaitAndReturnTime(5, "Подогревает вчерашний ужин");
+    totalTime += WaitAndReturnTime(10, "Набирает ванну");
+    totalTime += WaitAndReturnTime(15, "Принимает ванну");
+    totalTime += WaitAndReturnTime(10, "Завтракает");
+    totalTime += WaitAndReturnTime(5, "Одевается");
+    totalTime += WaitAndReturnTime(55, "Едет на работу");
+}
+
+int WaitAndReturnTime(int minutes, string action)
+{
+    int ms = minutes * 100; 
+    Thread.Sleep(ms); 
+    Console.WriteLine($"Затрачено времени на {action}: {minutes} минут");
+    return minutes;
+}
